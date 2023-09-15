87456203aa3370e9e27fa5ad2a4bd8a7258b7ee9
Simonzuleta <133362524+Simonzuleta@users.noreply.github.com>
2023-09-15 09:50:06 -0500

    Update roleRoutes.js

--- src/routes/roleRoutes.js
+++ src/routes/roleRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const RoleController = require('../controllers/RoleController');
 
 // Ruta para obtener todos los roles
-router.get('/', RoleController.getAllRoles);
+router.get('/rol', RoleController.getAllRoles);
 
 // Ruta para obtener un rol por ID
-router.get('/:id', RoleController.getRoleById);
+router.get('/rol:id', RoleController.getRoleById);
 
 // Ruta para crear un nuevo rol
-router.post('/', RoleController.createRole);
+router.post('/rol', RoleController.createRole);
 
 // Ruta para actualizar un rol por ID
-router.put('/:id', RoleController.updateRole);
+router.put('/rol:id', RoleController.updateRole);
 
 // Ruta para eliminar un rol por ID
-router.delete('/:id', RoleController.deleteRole);
+router.delete('/rol:id', RoleController.deleteRole);
 
 module.exports = router;
